9e50cd0abe2d321944998883aaca629fb0409329
arvan903 <arvan903@hotmail.com>
2022-03-26 23:40:47 +0430

    now at the start of the app permission request will popup

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -5,6 +5,14 @@ class Location {
   late double longitude;
 
   Future<void> getCurrentLocation() async {
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.low);
